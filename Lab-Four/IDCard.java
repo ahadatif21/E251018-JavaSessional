96db8589394b73adbb2b026ea1b3ccd1cdc0d8f4
Ahad Atif <atif670843@gmail.com>
2025-12-09 23:29:03 +0600

    Create IDCard.java

---
+++ Lab-Four/IDCard.java
@@ -0,0 +1,34 @@
+class IDCard {
+    String name;
+    String id;
+    String department;
+    String institution;
+
+    IDCard(String name, String id, String department, String institution) {
+        this.name = name;
+        this.id = id;
+        this.department = department;
+        this.institution = institution;
+    }
+
+    void showID() {
+        System.out.println("ID Card Information:");
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Department: " + department);
+        System.out.println("Institution: " + institution);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        IDCard student = new IDCard(
+            "Mintaj Miul Ahad Atif",
+            "E251018",
+            "CCE",
+            "IIUC"
+        );
+        
+        student.showID();
+    }
+}
